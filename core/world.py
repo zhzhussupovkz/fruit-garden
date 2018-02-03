c88a8177ab929cf52e1d760926b86051e3f405a1
zhzhussupovkz <zhzhussupovkz@gmail.com>
2018-02-04 00:10:39 +0600

    add pause event to game

--- core/world.py
+++ core/world.py
@@ -13,6 +13,7 @@ class World():
         self.icon_img = pygame.image.load("./images/levels/game.png")
         pygame.display.set_caption('Fruit garden')
         pygame.display.set_icon(self.icon_img)
+        self.pause = False
         self.pygame = pygame
         self.screen = pygame.display.set_mode(self.SIZE)
         self.level = Level(self, 1)
@@ -21,13 +22,17 @@ class World():
         self.level.draw()
 
     def update(self):
-        self.level.update()
+        if self.pause == False:
+            self.level.update()
 
     def play(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_p:
+                        self.pause = not self.pause
             key = pygame.key.get_pressed()
             if key[pygame.K_ESCAPE]:
                 sys.exit()
